c90be2f9eaa816c38883e1b1929221067d06d1b7
LisbethMendoza <lisbethango223019@gmail.com>
2024-10-05 16:44:40 -0400

    Basic variables, instances and print

--- main.dart
+++ main.dart
@@ -1,5 +1,23 @@
 // variables, constants and operators is the same like javascrip and always in a class
 
 class variables {
-  String hola = "hola mundo";
+  int edad = 25;
+  String name = 'Juan';
+  double height = 1.75;
+}
+
+void main() {
+  // Crete instances for you uses your class
+  variables myObject = variables();
+  print(myObject.name);
+  print(myObject.height);
+}
+
+//Only constants can be out of class
+const pi = 3.1416;
+final datenow = DateTime.now();
+
+void const_and_date() {
+  print(pi);
+  print(datenow);
 }
