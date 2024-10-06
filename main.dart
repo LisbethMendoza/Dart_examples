876c78c02720da559958c03e28c3ef0ed34cfe57
LisbethMendoza <lisbethango223019@gmail.com>
2024-10-05 22:31:02 -0400

    Funcion, variables null and private property

--- main.dart
+++ main.dart
@@ -10,9 +10,11 @@ void main() {
   print(result2);
 
   var myclass = MyClass('Lisbeth');
-  print('The value of myObject is: $myclass'); 
+  print('The value of myObject is: $myclass');
 
-
+  sayHello();
+  const_and_date();
+  greet('Lisbeth');
 }
 
 class variables {
@@ -39,10 +41,8 @@ void interpolation() {
   String name1 = '${"Maria is " + 2.toString() + " years old"}';
   print(name1);
 
-  String name2 = '${"Maria is " + 'very smart'.toUpperCase()+ " years old"}';
+  String name2 = '${"Maria is " + 'very smart'.toUpperCase() + " years old"}';
   print(name2);
-  
-
 
 //that is when you want to change the form of somtthing, do you wanna put 2 variables, etc
   String result2 = '${"example".substring(0, 4)}';
@@ -61,6 +61,38 @@ class MyClass {
   }
 }
 
-
 //---------------------------------------------------------------------------------------//
-//Funtion in Dart
+//Funtion in without parameters
+void sayHello() {
+  print('Hello!');
+}
+
+// Funtion with parameters
+void greet(String name) {
+  print('Hello, $name!');
+}
+
+int add(int a, int b) {
+  return a + b;
+}
+
+void describe(String name, [int age = 0]) {
+  print('Name: $name, Age: $age');
+}
+
+// Null in dart can be identive with ?
+int? a; //that is null
+//a ??= 3;  before was null but now your value is 3
+//print(1 ?? 3); that want to say. You have the firt paramer if this it isn't null, but if that is null you have the other paramer.
+
+//Class with private property
+class private_property {
+  int _aProperty = 0; //you only can used that in this class
+  int get aProperty => _aProperty; // you can use this value get
+
+  set aProperty(int value) {  // with tha form you can change the firt private property
+    if (value >= 0) {
+      _aProperty = value;
+    }
+  }
+}
